2369a8e87895bcf0df06595aaee9f2061d4b98d0
JordyLimaEng <jordy.sousa@eng.ci.ufpb.br>
2020-05-15 03:53:10 -0300

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -39,42 +42,40 @@ public class SellerDaoJDBC implements SellerDao {
 	}
 
 	@Override
-	public Seller findById(Integer id) {//retornar seller por id
+	public Seller findById(Integer id) {// retornar seller por id
 		PreparedStatement st = null;
 		ResultSet rs = null;
-		
+
 		try {
 			st = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName "
-					+ "FROM seller INNER JOIN department "
-					+ "ON seller.DepartmentId = department.Id "
-					+ "WHERE seller.Id = ?"
-					);
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "WHERE seller.Id = ?");
 			st.setInt(1, id);
-			rs = st.executeQuery();//assim que recebe, aponta para a pos 0, que n tem nada
-			
-			if(rs.next()) {//se veio algum resultado na requisição
-				//instanciar dept
+			rs = st.executeQuery();// assim que recebe, aponta para a pos 0, que n tem nada
+
+			if (rs.next()) {// se veio algum resultado na requisição
+				// instanciar dept
 				Department dep = instantiateDepartment(rs);
-				//instanciar seller
+				// instanciar seller
 				Seller Obj = instantiateSeller(rs, dep);
-				//devolve o objeto com as requisições
+				// devolve o objeto com as requisições
 				return Obj;
 			}
-			
-			return null;//se não recebeu nada, retorna nuull
-		}catch(SQLException e) {
+
+			return null;// se não recebeu nada, retorna nuull
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}finally {
+		} finally {
 			DB.closeStatement(st);
-			DB.closeResultSet(rs);;
+			DB.closeResultSet(rs);
+			;
 		}
-		
-		
 	}
 
-	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {//como a fução é insanciada em um local em que já é tratado a exception
-																						//Aqui não é tratada a exceção
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {// como a fução é insanciada em
+																						// um local em que já é tratado
+																						// a exception
+																						// Aqui não é tratada a exceção
 		Seller Obj = new Seller();
 		Obj.setId(rs.getInt("Id"));
 		Obj.setName(rs.getString("Name"));
@@ -85,8 +86,9 @@ public class SellerDaoJDBC implements SellerDao {
 		return Obj;
 	}
 
-	private Department instantiateDepartment(ResultSet rs) throws SQLException {//como a fução é insanciada em um local em que já é tratado a exception
-																				//Aqui não é tratada a exceção
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {// como a fução é insanciada em um local
+																				// em que já é tratado a exception
+																				// Aqui não é tratada a exceção
 		Department dep = new Department();
 		dep.setId(rs.getInt("DepartmentId"));
 		dep.setName(rs.getString("DepName"));
@@ -97,6 +99,51 @@ public class SellerDaoJDBC implements SellerDao {
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName " 
+							+ "FROM seller INNER JOIN department "							
+							+ "ON seller.DepartmentId = department.Id " 
+							+ "WHERE DepartmentId = ? " 
+							+ "ORDER BY Name");
+			st.setInt(1, department.getId());
+			rs = st.executeQuery();// assim que recebe, aponta para a pos 0, que n tem nada
+
+			List<Seller> list = new ArrayList<>();
+			
+			Map<Integer, Department> map = new HashMap<>(); // map aqui serve para evitar que as requisições venham
+															// em obj's separados, mas sim atrelados
+															//guarda todos os depts q forem instanciados
+
+			while (rs.next()) {// pode ser mais ou igual a zero, logo temos q percorrer
+				// verifica se o dep ja existe no map, se não existe, retorna nulo.
+				Department dep = map.get(rs.getInt("DepartmentId"));
+
+				if (dep == null) {// não existem logo instancia
+					dep = instantiateDepartment(rs);
+					// salva dep com a key sendo a id do departamento
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+
+				// instanciar seller
+				Seller Obj = instantiateSeller(rs, dep);
+				// devolve o objeto com as requisições
+				list.add(Obj);
+			}
+			return list;// se não recebeu nada, retorna nuull
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 }
